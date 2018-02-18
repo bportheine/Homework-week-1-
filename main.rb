b04d3ab49209b7e99d96d03b0e4a9feb2a5a48fb
Brandon Portheine <bportheine@gmail.com>
2018-02-18 14:04:55 +0100

    map contact name and phone to key value pair respectively

--- main.rb
+++ main.rb
@@ -4,3 +4,7 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+
+contacts = read_contacts
+print contacts.map {|contact| {contact[:name] => contact[:phone]}}
