5d477170a4e0281b7fdd9afa4944a356746cc052
Brandon Portheine <bportheine@gmail.com>
2018-02-18 15:23:13 +0100

    Add select phone and email methods

--- main.rb
+++ main.rb
@@ -5,9 +5,25 @@ def read_contacts
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
 
-def map
+def contact_map
   contacts = read_contacts
+  puts
   print contacts.map {|contact| {contact[:name] => contact[:phone]}}
 end
 
-map
+def contact_select_phone
+  contacts = read_contacts
+  puts
+  print contacts.select {|contact| contact[:phone].include? "+1"}
+end
+
+def contact_select_email
+  contacts = read_contacts
+  puts
+  print contacts.select {|contact| contact[:email].include? ".org"}
+end
+
+puts
+contact_map
+contact_select_phone
+contact_select_email
